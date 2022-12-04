94234b3494fed4f1fa85f3ad22100b417d6d9448
Kevin Sander <kevintsander@gmail.com>
2022-12-04 15:55:24 -0500

    added paginator

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,6 +2,6 @@ class FlightsController < ApplicationController
   def index
     @flights = Flight.search({ departure_airport_id: params[:departure_airport_id],
                                arrival_airport_id: params[:arrival_airport_id],
-                               depart_date: params[:depart_date] })
+                               depart_date: params[:depart_date] }).page params[:page]
   end
 end
