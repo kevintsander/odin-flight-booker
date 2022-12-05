fec8e52095f89152e46e7e5f7207d18ecb5bad02
Kevin Sander <kevintsander@gmail.com>
2022-12-05 09:47:11 -0500

    allow blank search fields

--- lib/shared_methods.rb
+++ lib/shared_methods.rb
@@ -1,6 +1,7 @@
 module SharedMethods
   def self.date_from_param_parts(params, key)
     return unless params["#{key}(1i)"] && params["#{key}(2i)"] && params["#{key}(3i)"]
+    return if params["#{key}(1i)"].empty? || params["#{key}(2i)"].empty? || params["#{key}(3i)"].empty?
 
     Date.new(params["#{key}(1i)"].to_i,
              params["#{key}(2i)"].to_i,
