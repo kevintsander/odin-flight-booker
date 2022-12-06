a60199ca5178b159192667d597c5a3410b961a04
Kevin Sander <kevintsander@gmail.com>
2022-12-06 18:09:49 -0500

    added flight select radio button & number of passengers

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,5 +4,7 @@ class FlightsController < ApplicationController
                                arrival_airport_id: params[:arrival_airport_id],
                                depart_date: SharedMethods.date_from_param_parts(params,
                                                                                 'depart_date') }).page params[:page]
+
+    @number_of_passengers = params[:number_of_passengers]
   end
 end
