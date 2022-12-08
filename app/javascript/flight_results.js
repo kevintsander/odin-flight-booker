cd440a3e8ad6da7eabe332a25a141973e85d099c
Kevin Sander <kevintsander@gmail.com>
2022-12-08 00:40:37 -0500

    updated javascript to reload correctly with turbo

--- app/javascript/flight_results.js
+++ app/javascript/flight_results.js
@@ -1,8 +1,35 @@
-const flight_cards = document.querySelectorAll(".flight-card");
+document.addEventListener("turbo:load", setupFlightCardListeners);
 
-flight_cards.forEach((card) => {
-  card.addEventListener("click", () => {
+function setupFlightCardListeners() {
+  const flight_cards = document.querySelectorAll(".flight-card");
+
+  flight_cards.forEach((card) => {
     let radio_button = card.querySelector("input[type='radio']");
-    radio_button.checked = !radio_button.checked;
+
+    function change_selected_flight() {
+      if (radio_button.checked) {
+        radio_button.checked = false;
+        card.classList.remove("flight-card-selected");
+      } else {
+        radio_button.checked = true;
+        card.classList.add("flight-card-selected");
+        flight_cards.forEach((otherCard) => {
+          if (card != otherCard) {
+            otherCard.classList.remove("flight-card-selected");
+          }
+        });
+      }
+    }
+
+    radio_button.addEventListener("change", (e) => {
+      change_selected_flight();
+      e.stopPropagation();
+    });
+
+    card.addEventListener("click", (e) => {
+      change_selected_flight();
+    });
   });
-});
+}
+
+setupFlightCardListeners();
