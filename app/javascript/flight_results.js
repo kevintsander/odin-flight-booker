bc21d6c5ac25ee6cac09fe8fe0339f3d97299780
Kevin Sander <kevintsander@gmail.com>
2022-12-19 12:45:46 -0500

    use label instead of javascript to select child radio button

--- app/javascript/flight_results.js
+++ app/javascript/flight_results.js
@@ -1,35 +1,35 @@
-document.addEventListener("turbo:load", setupFlightCardListeners);
+// document.addEventListener("turbo:load", setupFlightCardListeners);
 
-function setupFlightCardListeners() {
-  const flight_cards = document.querySelectorAll(".flight-card");
+// function setupFlightCardListeners() {
+//   const flight_cards = document.querySelectorAll(".flight-card");
 
-  flight_cards.forEach((card) => {
-    let radio_button = card.querySelector("input[type='radio']");
+//   flight_cards.forEach((card) => {
+//     let radio_button = card.querySelector("input[type='radio']");
 
-    function change_selected_flight() {
-      if (radio_button.checked) {
-        radio_button.checked = false;
-        card.classList.remove("flight-card-selected");
-      } else {
-        radio_button.checked = true;
-        card.classList.add("flight-card-selected");
-        flight_cards.forEach((otherCard) => {
-          if (card != otherCard) {
-            otherCard.classList.remove("flight-card-selected");
-          }
-        });
-      }
-    }
+//     function change_selected_flight() {
+//       if (radio_button.checked) {
+//         radio_button.checked = false;
+//         card.classList.remove("flight-card-selected");
+//       } else {
+//         radio_button.checked = true;
+//         card.classList.add("flight-card-selected");
+//         flight_cards.forEach((otherCard) => {
+//           if (card != otherCard) {
+//             otherCard.classList.remove("flight-card-selected");
+//           }
+//         });
+//       }
+//     }
 
-    radio_button.addEventListener("change", (e) => {
-      change_selected_flight();
-      e.stopPropagation();
-    });
+//     radio_button.addEventListener("change", (e) => {
+//       change_selected_flight();
+//       e.stopPropagation();
+//     });
 
-    card.addEventListener("click", (e) => {
-      change_selected_flight();
-    });
-  });
-}
+//     card.addEventListener("click", (e) => {
+//       change_selected_flight();
+//     });
+//   });
+// }
 
-setupFlightCardListeners();
+// setupFlightCardListeners();
